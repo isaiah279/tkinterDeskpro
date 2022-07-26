37fc8e7ea276c4d57eb6eec02bc8f78d9c4ee60d
Isayah Mboya <81564990+isaiah279@users.noreply.github.com>
2022-07-26 15:34:05 +0300

    secondcommit

--- main2.py
+++ main2.py
@@ -60,10 +60,10 @@ class FirstPage(tk.Frame):
             window.configure(bg="deep sky blue")
             window.resizable(0, 0)
 
-            username_label = tk.Label(window, text="Username", font=("Arial", 15))
-            username_label.place(x=10, y=10)
-            username_ent = tk.Entry(window, width=30, bd=0)
-            username_ent.place(x=200, y=10)
+            username_label = tk.Label(window, text="Username", font=("Arial", 15),bg="blue")
+            username_label.place(x=10, y=10,ipadx=30,ipady=10)
+            username_ent = tk.Entry(window, width=30, bd=0,ipady=100)
+            username_ent.place(x=200, y=10,ipady=100)
 
             password_label = tk.Label(window, text="Password", font=("Arial", 15))
             password_label.place(x=10, y=50)
@@ -95,6 +95,8 @@ class FirstPage(tk.Frame):
 
         Next_btn = tk.Button(self, text="Next", font=("Arial", 15), command=lambda: controller.show_frame(SecondPage))
         Next_btn.place(x=750, y=0)
+        finder = tk.Label(self, text="First Page")
+        finder.place(x=1250, y=0)
 
 
 class SecondPage(tk.Frame):
@@ -103,38 +105,57 @@ class SecondPage(tk.Frame):
         L2 = tk.Label(self, text="my Second Page", font=("Arial Bold", 30))
         L2.grid(row=2, column=3)  # x=200, y=230
 
-        Button = tk.Button(self, text="Back", font=("Arial", 15), command=lambda: controller.show_frame(FirstPage))
+        Button = tk.Button(self, text="First Page", font=("Arial", 15),
+                           command=lambda: controller.show_frame(FirstPage))
         Button.grid(row=5, column=5)  # x=100, y=450
 
-        Button = tk.Button(self, text="Next", font=("Arial", 15), command=lambda: controller.show_frame(ThirdPage))
+        Button = tk.Button(self, text="Third Page", font=("Arial", 15),
+                           command=lambda: controller.show_frame(ThirdPage))
         Button.grid(row=5, column=8)
+        Button = tk.Button(self, text="Administrator", font=("Arial", 15),
+                           command=lambda: controller.show_frame(AdminPage))
+        Button.grid(row=5, column=8)
+        Button = tk.Button(self, text="Fouth Page", font=("Arial", 15),
+                           command=lambda: controller.show_frame(FouthPage))
+        Button.grid(row=5, column=20)
+        finder = tk.Label(self, text="Second Page")
+        finder.place(x=1250, y=0)
 
 
 class ThirdPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         # def comments():
+        self.configure(bg="deep sky blue")
 
         na = tk.Label(text="The commenting page of the System", font=('Helvetica', 26, 'bold'))
         na.pack(pady=20)
-        wrapper1 = tk.LabelFrame(self, text="Comments", fg="red")
-        wrapper2 = tk.LabelFrame(self, text="Actions", fg="red")
-
-        wrapper1.pack(fill="both", expand="true", padx=40, pady=70)
-        wrapper2.pack(fill="both", expand="true", padx=40, pady=10, ipady=20)
-        lcoment = tk.Label(wrapper1, text=" Actions")
-        textarea = tk.Text(wrapper1, width=30, height=8, font=20, fg="green")
+        wrapper1 = tk.LabelFrame(self, text="Write Comment", fg="White",bg="dark blue",font=('Arial',14),border=10)
+        wrapper1.pack(fill="both", expand="true", padx=0, pady=80,ipady=2)
+        textarea = tk.Text(wrapper1, width=25, height=15, font=20, fg="green")
         textarea.pack(ipady=50, ipadx=200, anchor='center', pady=20)
 
-        Button = tk.Button(self, text="Home", font=("Arial", 15), command=lambda: controller.show_frame(FirstPage))
-        Button.place(x=0, y=30)
-        Button = tk.Button(self, text="FirstPage", font=("Arial", 15), command=lambda: controller.show_frame(FirstPage))
-        Button.place(x=100, y=30)
-        Button = tk.Button(self, text="Navigate", font=("Arial", 15), command=lambda: controller.show_frame(SecondPage))
-        Button.place(x=200, y=30)
+        #action buttons
+        send_btn=tk.Button(wrapper1,text="send Comment")
+        send_btn.place(x=350,y=450)
+        clear_btn = tk.Button(wrapper1, text="Clear Comment")
+        clear_btn.place(x=500,y=450)
+        clear_btn = tk.Button(wrapper1, text="Exit page")
+        clear_btn.place(x=650, y=450)
 
-        Button = tk.Button(self, text="Next", font=("Arial", 15), command=lambda: controller.show_frame(FouthPage))
-        Button.place(x=300, y=30)  # x=100, y=450
+        Button = tk.Button(self, text="Home", font=("Arial", 15),bg='blue', command=lambda: controller.show_frame(FirstPage))
+        Button.place(x=5, y=15)
+        Button = tk.Button(self, text="FirstPage", font=("Arial", 15),bg='blue',  command=lambda: controller.show_frame(FirstPage))
+        Button.place(x=80, y=15)
+        Button = tk.Button(self, text="Navigate", font=("Arial", 15),bg='blue',  command=lambda: controller.show_frame(SecondPage))
+        Button.place(x=190, y=15)
+
+        Button = tk.Button(self, text="FouthPage", font=("Arial", 15),bg='blue',  command=lambda: controller.show_frame(FouthPage))
+        Button.place(x=295, y=15)  # x=100, y=450
+
+
+        finder = tk.Label(self, text="Third Page")
+        finder.place(x=1250, y=0)
 
 
 class FouthPage(tk.Frame):
@@ -146,30 +167,30 @@ class FouthPage(tk.Frame):
         def add_information():
             pass
 
-        def delete_information():
+        def clear_information():
             pass
 
         def cancel_information():
             pass
 
         # phone, email, name, location, issue
-        wrapper1 = tk.LabelFrame(self, text="Enter Details",bg="blue")
+        wrapper1 = tk.LabelFrame(self, text="Enter Details", bg="blue")
         wrapper2 = tk.LabelFrame(self, text="Actions Required")
 
         wrapper2.config(bg="green")
-        wrapper1.pack(fill="both", expand='yes',pady=50)
+        wrapper1.pack(fill="both", expand='yes', pady=50)
 
         # actions
-        add_btn = tk.Button(wrapper1, text="Add New", command=add_information)
-        delete_btn = tk.Button(wrapper1, text="Delete", command=delete_information)
+        add_btn = tk.Button(wrapper1, text="Submit Request", command=add_information)
+        delete_btn = tk.Button(wrapper1, text="Clear", command=clear_information)
         cancel_btn = tk.Button(wrapper1, text="Cancel", command=cancel_information)
 
         # labels
-        lname = tk.Label(wrapper1, text="Name:")
-        lemail = tk.Label(wrapper1, text="Email:")
-        lphone = tk.Label(wrapper1, text="Phone:")
-        lissue = tk.Label(wrapper1, text="Issue:")
-        llocation = tk.Label(wrapper1, text="Location")
+        lname = tk.Label(wrapper1, text="Name:",bg='blue')
+        lemail = tk.Label(wrapper1, text="Email:",bg='blue')
+        lphone = tk.Label(wrapper1, text="Phone:",bg='blue')
+        lissue = tk.Label(wrapper1, text="Issue:",bg='blue')
+        llocation = tk.Label(wrapper1, text="Location",bg='blue')
 
         # grids
         lname.grid(row=1, column=0, padx=20, pady=60)
@@ -184,20 +205,40 @@ class FouthPage(tk.Frame):
         elocation = tk.Entry(wrapper1)
         eissue = tk.Entry(wrapper1)
 
-        ename.grid(row=1, column=1, ipadx=20, ipady=5)
-        eemail.grid(row=2, column=1, ipady=5, ipadx=20)
-        ephone.grid(row=1, column=4, ipady=5, ipadx=20)
-        eissue.grid(row=2, column=4, ipady=5, ipadx=20)
-        elocation.grid(row=3, column=1, ipady=5, ipadx=20)
+        ename.grid(row=1, column=1, ipady=5, ipadx=60)
+        eemail.grid(row=2, column=1, ipady=5, ipadx=60)
+        ephone.grid(row=1, column=4, ipady=5, ipadx=60)
+        eissue.grid(row=2, column=4, ipady=5, ipadx=60)
+        elocation.grid(row=3, column=1, ipady=5, ipadx=60)
 
-        add_btn.grid(row=4, column=2, padx=5, pady=3)
-        delete_btn.grid(row=4, column=4, padx=5, pady=3)
-        cancel_btn.grid(row=4, column=6, padx=5, pady=3)
+        add_btn.grid(row=4, column=1,pady=40,ipadx=20)
+        delete_btn.grid(row=4, column=2,pady=40,ipadx=20)
+        cancel_btn.grid(row=4, column=3,pady=40,ipadx=20,padx=5)
 
-        Button = tk.Button(wrapper1, text="Back", font=("Arial", 15), command=lambda: controller.show_frame(ThirdPage))
-        Button.place(x=20, y=20)  # x=100, y=450
-        heading = tk.Label(self, text="New page")
-        heading.place(x=100, y=233)
+        Button = tk.Button(self, text="Third Page", font=("Arial", 15),bg="dark blue",command=lambda: controller.show_frame(FirstPage))
+        Button.place(x=0, y=5)  # x=100, y=450
+        Button = tk.Button(self, text="Third Page", font=("Arial", 15),bg="dark blue", command=lambda: controller.show_frame(SecondPage))
+        Button.place(x=120, y=5)
+        Button = tk.Button(self, text="Third Page", font=("Arial", 15),bg="dark blue",  command=lambda: controller.show_frame(ThirdPage))
+        Button.place(x=240, y=5)
+        Button = tk.Button(self, text="Admin Page", font=("Arial", 15),bg="dark blue", command=lambda: controller.show_frame(AdminPage))
+        Button.place(x=360, y=5)
+
+
+        finder = tk.Label(self, text="Fouth Page")
+        finder.place(x=1250, y=0)
+
+
+class AdminPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # def comments():
+        Button = tk.Button(self, text="Second page", font=("Arial", 15),
+                           command=lambda: controller.show_frame(SecondPage))
+        Button.place(x=0, y=0)
+        self.configure(bg="blue")
+        self.textlabel = tk.Label(self, text="Administration page", font=("arial", 30, 'bold'), bg='blue')
+        self.textlabel.place(x=400, y=10)
 
 
 class Application(tk.Tk):
@@ -210,11 +251,11 @@ class Application(tk.Tk):
         window.grid_columnconfigure(0, minsize=1365)
 
         self.frames = {}
-        for F in (FouthPage, ThirdPage, SecondPage, ThirdPage, FirstPage):
+        for F in ( ThirdPage,FouthPage,AdminPage, SecondPage, ThirdPage, FirstPage):
             frame = F(window, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame(FouthPage)
+        self.show_frame( ThirdPage)
 
     def show_frame(self, page):
         frame = self.frames[page]
